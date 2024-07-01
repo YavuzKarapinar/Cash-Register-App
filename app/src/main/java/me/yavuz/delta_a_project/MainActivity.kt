df06729ef51f5261ad760c20c630fb1442d9e702
YavuzKarapinar <ext.y.s.karapinar@delta-ac.de>
2024-07-01 12:12:23 +0300

    Initial Commit

---
+++ app/src/main/java/me/yavuz/delta_a_project/MainActivity.kt
@@ -0,0 +1,27 @@
+package me.yavuz.delta_a_project
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var dbHelper: DbHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        dbHelper = DbHelper(this)
+
+    }
+}
